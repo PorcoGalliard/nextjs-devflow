ddfd05b5ac764ec466558f2aa44786d0a946bcec
Suryana <nanasuryana335@gmail.com>
2023-11-20 00:13:37 +0700

    feat(shared/LeftSideBar.tsx): implement LeftSideBar.tsx

---
+++ devflow/frontend/components/shared/LeftSideBar.tsx
@@ -0,0 +1,63 @@
+"use client";
+
+import { sidebarLinks } from "@/constants";
+import { SignedOut } from "@clerk/nextjs";
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import React from "react";
+import { Button } from "../ui/button";
+
+const LeftSideBar = () => {
+  const pathname = usePathname();
+
+  return (
+    <section className="background-light900_dark200 custom-scrollbar light-border shadow-light-300 sticky left-0 top-0 flex h-screen flex-col justify-between overflow-y-auto border-r p-6 pt-36 dark:shadow-none max-sm:hidden lg:w-[266px]">
+      <div className="flex flex-col gap-6">
+        {sidebarLinks.map((item) => {
+          const isActive =
+            (pathname.includes(item.route) && item.route.length > 1) ||
+            pathname === item.route;
+
+          return (
+            <Link
+              key={item.route}
+              href={item.route}
+              className={`${
+                isActive
+                  ? "primary-gradient text-light-900 rounded-lg"
+                  : "text-dark300_light900"
+              } flex items-center justify-start gap-4 bg-transparent p-4`}
+            >
+              <Image
+                src={item.imgURL}
+                alt={item.label}
+                height={20}
+                width={20}
+                className={`${isActive ? " " : "invert-colors"}`}
+              />
+              <p className={`${isActive ? "base-bold" : "base-medium"}`}>
+                {item.label}
+              </p>
+            </Link>
+          );
+        })}
+
+        <SignedOut>
+          <Link href="/sign-in">
+            <Button className="small-medium btn-secondary min-h-[41px] w-full rounded-lg px-4 py-3 shadow-none">
+              <span className="primary-text-gradient">Log in</span>
+            </Button>
+          </Link>
+          <Link href="/sign-up">
+            <Button className="small-medium light-border-2 text-dark400_light900 btn-tertiary min-h-[41px] w-full rounded-lg px-4 py-3 shadow-none">
+              <span>Sign Up</span>
+            </Button>
+          </Link>
+        </SignedOut>
+      </div>
+    </section>
+  );
+};
+
+export default LeftSideBar;
