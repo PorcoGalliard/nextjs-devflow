7afface4180deb4f4ea29c0740ffe0740e5a127f
Suryana <nanasuryana335@gmail.com>
2023-11-21 15:31:55 +0700

    feat(shared/search): implement LocalSearch component

---
+++ devflow/frontend/components/shared/search/LocalSearch.tsx
@@ -0,0 +1,48 @@
+"use client"
+
+import { Input } from "@/components/ui/input";
+import Image from "next/image";
+import React from "react";
+
+interface CustomInputProps {
+  route: string;
+  iconPosition: string;
+  imgSrc: string;
+  otherClasses?: string;
+  placeholder: string;
+}
+
+const LocalSearch = ({
+  route,
+  iconPosition,
+  imgSrc,
+  otherClasses,
+  placeholder,
+}: CustomInputProps) => {
+  return (
+    <div className="relative w-full max-lg:hidden">
+      <div
+        className={`background-light800_darkgradient relative flex min-h-[56px] grow items-center gap-4 rounded-[10px] px-4 ${otherClasses}`}
+      >
+        {iconPosition === "left" && (
+          <Image
+            src={imgSrc}
+            alt="local search"
+            width={24}
+            height={24}
+            className="cursor-pointer"
+          />
+        )}
+        <Input
+          type="text"
+          placeholder={placeholder}
+          value=""
+          onChange={() => {}}
+          className="placeholder paragraph-regular no-focus text-dark400_light700 flex border-none bg-transparent py-3 shadow-none outline-none"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default LocalSearch;
