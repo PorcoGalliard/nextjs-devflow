5b67b02e1260d48bec9782c08bb07b824a6f32c0
Suryana <nanasuryana335@gmail.com>
2023-11-29 23:10:25 +0700

    feat(api/question_handler): implement HandleAskQuestion in question_handler.go

--- devflow/backend/api/question_handler.go
+++ devflow/backend/api/question_handler.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 
 	"github.com/fullstack/dev-overflow/db"
+	"github.com/fullstack/dev-overflow/types"
 	"github.com/gofiber/fiber/v2"
 	"go.mongodb.org/mongo-driver/mongo"
 )
@@ -34,3 +35,28 @@ func (h *QuestionHandler) HandleGetQuestionByID(ctx *fiber.Ctx) error {
 	return ctx.JSON(question)
 }
 
+func (h *QuestionHandler) HandleAskQuestion (ctx *fiber.Ctx) error {
+	var params types.AskQuestionParams
+	if err := ctx.BodyParser(&params); err != nil {
+		return ErrBadRequest()
+	}
+
+	if errors := params.Validate(); len(errors) > 0 {
+		return ctx.JSON(errors)
+	}
+
+	question := &types.Question{
+		Title: params.Title,
+		Description: params.Description,
+		UserID: params.UserID,
+		Tags: params.Tags,
+		CreatedAt: params.CreatedAt,
+	}
+
+	insertedQuestion, err := h.questionStore.AskQuestion(ctx.Context(), question)
+	if err != nil {
+		return ErrBadRequest()
+	}
+	return ctx.JSON(insertedQuestion)
+
+}
