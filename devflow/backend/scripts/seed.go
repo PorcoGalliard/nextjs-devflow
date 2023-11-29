28957f5360eeebdd4c90c8a5abcf349d21dece35
Suryana <nanasuryana335@gmail.com>
2023-11-30 02:42:41 +0700

    feat(script/seed): add user and question for data testing in seed.go

--- devflow/backend/scripts/seed.go
+++ devflow/backend/scripts/seed.go
@@ -7,7 +7,6 @@ import (
 	"os"
 	"time"
 
-	"github.com/clerkinc/clerk-sdk-go/clerk"
 	"github.com/fullstack/dev-overflow/db"
 	"github.com/fullstack/dev-overflow/db/fixtures"
 	"github.com/fullstack/dev-overflow/types"
@@ -25,13 +24,7 @@ func main() {
 	var (
 		mongoDBEndpoint = os.Getenv("MONGO_DB_URL")
 		mongoDBName = os.Getenv("MONGO_DB_NAME")
-		clerkApiKey = os.Getenv("CLERK_SECRET_KEY")
 	)
-
-	clerkClient, err := clerk.NewClient(clerkApiKey)
-	if err != nil {
-		log.Fatal(err)
-	}
 
 	mongoClient, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(mongoDBEndpoint))
 	if err != nil {
@@ -49,14 +42,9 @@ func main() {
 		User: userStore,
 	}
 
-	clerkUser, err := clerkClient.Users().Read("user_2XUKLyYOZGc5jlzwdFMjyXlHrOw")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	question := fixtures.AddQuestion(store, "Bagaimana cara mengatur GOROOT", "ini adalah contoh deskripsi", clerkUser.ID, []types.Tag{}, time.Now())
-	fmt.Println("Pertanyaan berhasil ditambahkan, berikut adalah ID pertanyaan", question.ID)
-
 	user := fixtures.AddUser(store, "Higuruma", "Hiromu", "higuruma@gmail.com", "higurumahiromu123")
-	fmt.Println("User berhasil ditambahkan, berikut adalah ID user", user.ID)
+	fmt.Println("User berhasil ditambahkan, berikut adalah ID user =>", user.ID)
+
+	question := fixtures.AddQuestion(store, "Bagaimana cara mengatur GOROOT", "ini adalah contoh deskripsi", user.ID, []types.Tag{}, time.Now())
+	fmt.Println("Pertanyaan berhasil ditambahkan, berikut adalah ID pertanyaan =>", question.ID)
 }
