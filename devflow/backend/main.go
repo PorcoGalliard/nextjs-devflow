b18fad5a9e0440c96b26e3300e994d8d985d6b42
Suryana <nanasuryana335@gmail.com>
2023-11-29 09:05:08 +0700

    feat(main.go): setup the application to start running

--- devflow/backend/main.go
+++ devflow/backend/main.go
@@ -1,13 +1,39 @@
 package main
 
 import (
-	"flag"
-	"fmt"
+	"context"
+	"log"
+	"os"
 
-	"github.com/gofiber/fiber"
+	"github.com/fullstack/dev-overflow/api"
+	"github.com/fullstack/dev-overflow/db"
+	"github.com/gofiber/fiber/v2"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 func main() {
-	listenAddr := flag.String("listen Addr", ":5000", "using for opening the server")
-	flag.Parse()
+	mongoEndpoint := os.Getenv("MONGO_DB_URL")
+	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(mongoEndpoint))
+	if err != nil {
+		log.Fatal(err)
+	} 
+
+	var (
+		questionStore = db.NewMongoQuestionStore(client)
+
+		store = &db.Store{
+			Question: questionStore,
+		}
+
+		questionHandler = api.NewQuestionHandler(store.Question)
+		app = fiber.New()
+		apiv1 = app.Group("/api/v1")
+	)
+
+	apiv1.Get("/question/:id", questionHandler.HandleGetQuestionByID)
+	
+
+	listenAddr := os.Getenv("HTTP_LISTEN_ADDRESS")
+	app.Listen(listenAddr)
 }
