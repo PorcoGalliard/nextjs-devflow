02c5d27c27c2439bc49d0f2d4bd525658dab07ce
Suryana <nanasuryana335@gmail.com>
2023-11-30 04:14:37 +0700

    feat(ask-question/page.tsx): implement frontend and fetch API with axios of ask-question page

--- devflow/frontend/app/(root)/ask-question/page.tsx
+++ devflow/frontend/app/(root)/ask-question/page.tsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import Question from "@/components/forms/Question";
+import { auth } from "@clerk/nextjs";
+import axios from "axios";
+import { redirect } from "next/navigation";
+import React from "react";
 
-const AskQuestion = () => {
+const page = () => {
+  const { userId } = auth();
+
+  if (!userId) redirect("/sign-in");
+
+  axios.get(`http://localhost:5000/api/v1/user/${userId}`).then((response) => {
+    const mongoUser = response.data;
+    console.log(mongoUser);
+  });
+
   return (
-    <div>AskQuestion</div>
-  )
-}
+    <div>
+      <h1 className="h1-bold text-dark100_light900">Ask a Question</h1>
+      <div className="mt-9">
+        <Question />
+      </div>
+    </div>
+  );
+};
 
-export default AskQuestion
+export default page;
