7b20b12d6637bcbe326850821b7537c4558f3661
Suryana <nanasuryana335@gmail.com>
2023-12-01 02:44:00 +0700

    feat(main.go): use fiber/cors to give access the data to the frontend side

--- devflow/backend/main.go
+++ devflow/backend/main.go
@@ -8,8 +8,16 @@ import (
 	"github.com/fullstack/dev-overflow/api"
 	"github.com/fullstack/dev-overflow/db"
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/joho/godotenv"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+var (
+	config = fiber.Config{
+		ErrorHandler: api.ErrorHandler,
+	}
 )
 
 func main() {
@@ -21,19 +29,36 @@ func main() {
 
 	var (
 		questionStore = db.NewMongoQuestionStore(client)
+		userStore = db.NewMongoUserStore(client)
 
 		store = &db.Store{
 			Question: questionStore,
+			User: userStore,
 		}
 
-		questionHandler = api.NewQuestionHandler(store.Question)
-		app = fiber.New()
+		questionHandler = api.NewQuestionHandler(store.Question, store.User)
+		userHandler = api.NewUserHandler(store.User)
+		app = fiber.New(config)
+		auth = app.Group("/api")
 		apiv1 = app.Group("/api/v1")
 	)
 
-	apiv1.Get("/question/:id", questionHandler.HandleGetQuestionByID)
+	app.Use(cors.New())
+	// Question Handler
+	apiv1.Get("/question/:_id", questionHandler.HandleGetQuestionByID)
+	apiv1.Post("/ask-question", questionHandler.HandleAskQuestion)
+
+	// User Handler
+	auth.Post("/sign-up", userHandler.HandleSignUp)
+	apiv1.Get("/user/:clerkID", userHandler.HandleGetUserByID)
 	
 
 	listenAddr := os.Getenv("HTTP_LISTEN_ADDRESS")
 	app.Listen(listenAddr)
+}
+
+func init() {
+	if err := godotenv.Load(); err != nil {
+		log.Fatal(err)
+	}
 }
