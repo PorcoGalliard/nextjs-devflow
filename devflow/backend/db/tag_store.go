71d532f9936caab4b675c905445ceb9319955b0e
Suryana <nanasuryana335@gmail.com>
2023-12-01 10:38:35 +0700

    refactor(db/tag_store): format the tag before GetTagByName and CreateTag

--- devflow/backend/db/tag_store.go
+++ devflow/backend/db/tag_store.go
@@ -5,6 +5,7 @@ import (
 	"os"
 
 	"github.com/fullstack/dev-overflow/types"
+	"github.com/fullstack/dev-overflow/utils"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
@@ -28,6 +29,7 @@ func NewMongoTagStore(client *mongo.Client) *MongoTagStore {
 type TagStore interface {
 	CreateTag(context.Context, *types.Tag) (*types.Tag, error)
 	GetTagByID(context.Context, string) (*types.Tag, error)
+	GetTagByName(context.Context, string) (*types.Tag, error)
 }
 
 func (s *MongoTagStore) GetTagByID(ctx context.Context, id string) (*types.Tag, error) {
@@ -45,7 +47,18 @@ func (s *MongoTagStore) GetTagByID(ctx context.Context, id string) (*types.Tag, 
 	return &tag, nil
 }
 
+func (s *MongoTagStore) GetTagByName(ctx context.Context, name string) (*types.Tag, error) {
+	var tag types.Tag
+	name = utils.FormatTag(name)
+	if err := s.collection.FindOne(ctx, bson.M{"name": name}).Decode(&tag); err != nil {
+		return nil, err
+	}
+
+	return &tag, nil
+}
+
 func (s *MongoTagStore) CreateTag(c context.Context, tag *types.Tag) (*types.Tag, error) {
+	tag.Name = utils.FormatTag(tag.Name)
 	res, err := s.collection.InsertOne(c, tag)
 	if err != nil {
 		return nil , err
