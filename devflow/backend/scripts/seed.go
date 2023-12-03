057ed464343cf90a6f717f06917cd30082a285ed
Suryana <nanasuryana335@gmail.com>
2023-12-04 05:53:51 +0700

    feat(scripts/seed.go): implement addTag and updateTag in seed.go

--- devflow/backend/scripts/seed.go
+++ devflow/backend/scripts/seed.go
@@ -10,6 +10,7 @@ import (
 	"github.com/fullstack/dev-overflow/db"
 	"github.com/fullstack/dev-overflow/db/fixtures"
 	"github.com/fullstack/dev-overflow/types"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 
@@ -37,15 +38,30 @@ func main() {
 
 	questionStore := db.NewMongoQuestionStore(mongoClient)
 	userStore := db.NewMongoUserStore(mongoClient)
+	tagStore := db.NewMongoTagStore(mongoClient)
 	store := &db.Store{
 		Question: questionStore,
 		User: userStore,
+		Tag: tagStore,
 	}
 
 
 	user := fixtures.AddUser(store, "Higuruma", "Hiromi", "12345678", "higuruma@gmail.com", "higurumahiromu123")
 	fmt.Println("User berhasil ditambahkan, berikut adalah ID user =>", user.ID)
 
-	question := fixtures.AddQuestion(store, "Bagaimana cara mengatur GOROOT", "ini adalah contoh deskripsi", user.ID, []types.Tag{}, time.Now())
+	tag := fixtures.AddTag(store, "Golang")
+
+	question := fixtures.AddQuestion(store, "Bagaimana cara mengatur GOROOT", "ini adalah contoh deskripsi", user.ID, []types.Tag{*tag}, time.Now().UTC())
 	fmt.Println("Pertanyaan berhasil ditambahkan, berikut adalah ID pertanyaan =>", question.ID)
+	question2 := fixtures.AddQuestion(store, "Bagaimana cara mengatur GOROOT", "ini adalah contoh deskripsi", user.ID, []types.Tag{*tag}, time.Now().UTC())
+	fmt.Println("Pertanyaan berhasil ditambahkan, berikut adalah ID pertanyaan =>", question2.ID)
+
+	questions := []primitive.ObjectID{question.ID, question2.ID}
+	followers := []primitive.ObjectID{user.ID}
+
+	updatedTag, err := fixtures.UpdateTag(store, tag.ID, &types.UpdateTagQuestionAndFollowers{Questions: questions, Followers: followers})
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Tag berhasil diupdate =>", updatedTag.ID)
 }
