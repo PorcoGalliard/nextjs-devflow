d1b956be7a1befbe1b79cb604f5e8b068eb89eda
Suryana <nanasuryana335@gmail.com>
2023-12-04 01:39:04 +0700

    feat(api/tag_handler.go): implement handler for tag type logics

---
+++ devflow/backend/api/tag_handler.go
@@ -0,0 +1,73 @@
+package api
+
+import (
+	"errors"
+
+	"github.com/fullstack/dev-overflow/db"
+	"github.com/fullstack/dev-overflow/types"
+	"github.com/gofiber/fiber/v2"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+type TagHandler struct {
+	tagStore db.TagStore
+}
+
+func NewTagHandler(tagStore db.TagStore) *TagHandler {
+	return &TagHandler{
+		tagStore: tagStore,
+	}
+}
+
+func (h *TagHandler) HandleGetTagByID(ctx *fiber.Ctx) error {
+	var (
+		id = ctx.Params("_id")
+	)
+
+	tag, err := h.tagStore.GetTagByID(ctx.Context(), id)
+	if err != nil {
+		if errors.Is(err, mongo.ErrNoDocuments){
+			return ErrResourceNotFound(id)
+		}
+	}
+	return ctx.JSON(tag)
+
+}
+
+func (h *TagHandler) HandleGetTagByName(ctx *fiber.Ctx) error {
+	var (
+		name = ctx.Params("name")
+	)
+
+	tag, err := h.tagStore.GetTagByName(ctx.Context(), name)
+	if err != nil {
+		if errors.Is(err, mongo.ErrNoDocuments){
+			return ErrResourceNotFound(name)
+		}
+	}
+	return ctx.JSON(tag)
+}
+
+func (h *TagHandler) HandleCreateTag(ctx *fiber.Ctx) error {
+	var params types.CreateTagParams
+
+	if err := ctx.BodyParser(&params); err != nil {
+		return err
+	}
+
+	tag := &types.Tag{
+		Name: params.Name,
+	}
+
+	tag, err := h.tagStore.CreateTag(ctx.Context(), tag)
+	if err != nil {
+		return ErrBadRequest()
+	}
+
+	return ctx.JSON(tag)
+}
+
+func (h *TagHandler) HandleUpdateTag(ctx *fiber.Ctx) error {
+	// TODO:
+	return nil
+}
