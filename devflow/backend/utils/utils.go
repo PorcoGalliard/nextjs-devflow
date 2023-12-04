52bdfa92a103bdf095de18e1b34577a00622f3d6
Suryana <nanasuryana335@gmail.com>
2023-12-04 20:46:38 +0700

    refactor(utils.go): change every input of js and db to ToUpper

--- devflow/backend/utils/utils.go
+++ devflow/backend/utils/utils.go
@@ -1,6 +1,7 @@
 package utils
 
 import (
+	"regexp"
 	"strings"
 
 	"golang.org/x/text/cases"
@@ -9,6 +10,15 @@ import (
 
 func FormatTag(tag string) string {
 	tag = strings.ReplaceAll(strings.TrimSpace(tag), ".", " ")
+
+	tag = strings.ToLower(tag)
+
 	tag = cases.Title(language.English).String(tag)
+
+	re := regexp.MustCompile(`\b(js|db|Js|Db)\b`)
+	tag = re.ReplaceAllStringFunc(tag, func(s string) string {
+		return strings.ToUpper(s)
+	})
+
 	return tag
 }
