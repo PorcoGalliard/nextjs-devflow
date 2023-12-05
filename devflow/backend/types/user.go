c83d3431fc693ac4616fa78503beb95187cd5ecb
Suryana <nanasuryana335@gmail.com>
2023-12-05 19:20:50 +0700

    feat(types/user.go): create UpdateUserParam tag and comment out password field

--- devflow/backend/types/user.go
+++ devflow/backend/types/user.go
@@ -22,14 +22,14 @@ type User struct {
 	ClerkID string `bson:"clerkID" json:"clerkID"`
 	FirstName string `bson:"firstName" json:"firstName"`
 	LastName string `bson:"lastName" json:"lastName"`
-	Bio *string `bson:"bio" json:"bio"`
-	Picture *string `bson:"picture" json:"picture"` 
+	Bio string `bson:"bio" json:"bio"`
+	Picture string `bson:"picture" json:"picture"` 
 	Email string `bson:"email" json:"email"`
 	EncryptedPassword string `bson:"password" json:"-"`
-	Location *string `bson:"location" json:"location"`
-	PortfolioWebsite *string `bson:"portfolioWebsite" json:"portfolioWebsite"`
+	Location string `bson:"location" json:"location"`
+	PortfolioWebsite string `bson:"portfolioWebsite" json:"portfolioWebsite"`
 	IsAdmin bool `bson:"isAdmin" json:"isAdmin"`
-	Reputation *int `bson:"reputation" json:"reputation"`
+	Reputation int `bson:"reputation" json:"reputation"`
 	Saved []primitive.ObjectID `bson:"saved" json:"saved"`
 	JoinedAt time.Time `bson:"joinedAt" json:"joinedAt"`
 }
@@ -39,26 +39,27 @@ type CreateUserParam struct {
 	LastName string `json:"lastName"`
 	ClerkID string `json:"clerkID"`
 	Email string `json:"email"`
-	Password string `json:"password"`
+	// Password string `json:"password"`
+	Picture string `json:"picture"`
 }
 
 type UpdateUserParam struct {
-	FirstName string `json:"firstName"`
-	LastName string `json:"lastName"`
+	ClerkID string `json:"clerkID"`
+	UpdateData User `json:"updateData"`
 }
 
 func NewUserFromParams(params CreateUserParam) (*User, error) {
-	encpw, err := bcrypt.GenerateFromPassword([]byte(params.Password), bcryptCost)
-	if err != nil {
-		return nil, err
-	}
+	// encpw, err := bcrypt.GenerateFromPassword([]byte(params.Password), bcryptCost)
+	// if err != nil {
+	// 	return nil, err
+	// }
 
 	return &User{
 		FirstName: params.FirstName,
 		LastName: params.LastName,
 		ClerkID: params.ClerkID,
 		Email: params.Email,
-		EncryptedPassword: string(encpw),
+		// EncryptedPassword: string(encpw),
 		JoinedAt: time.Now().UTC(),
 	}, nil
 }
@@ -87,9 +88,9 @@ func (params CreateUserParam) Validate() map[string]string {
 		errors["Email"] = fmt.Sprintf("Your email %s is not a valid email", params.Email)
 	}
 
-	if len(params.Password) < minPasswordLen {
-		errors["Password"] = fmt.Sprintf("Password must be at least %d characters", minPasswordLen)
-	}
+	// if len(params.Password) < minPasswordLen {
+	// 	errors["Password"] = fmt.Sprintf("Password must be at least %d characters", minPasswordLen)
+	// }
 
 	return errors
 }
