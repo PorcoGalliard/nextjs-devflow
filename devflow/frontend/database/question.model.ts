20365eb44c6817d6f46bf10092d145d68bc5ac32
Suryana <nanasuryana335@gmail.com>
2023-12-05 23:57:58 +0700

    refactor(database/question.model): change answers type from Object to Answer[]

--- devflow/frontend/database/question.model.ts
+++ devflow/frontend/database/question.model.ts
@@ -1,14 +1,20 @@
+import Answer from "./answer.model";
+import Tag from "./tag.model";
+import User from "./user.model";
+
 type Question = {
   _id: string;
   title: string;
   description: string;
   userID: string;
+  user: User;
   tags: string[];
+  tagDetails: Tag[];
   views: number;
   upvotes: string[];
   downvotes: string[];
-  answers: string[]; //
-  createdAt: string; //
+  answers: Answer[]; //
+  createdAt: Date; //
 };
 
 export default Question;
