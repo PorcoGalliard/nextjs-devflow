6135a5532bce17337b955e728921d8dcd5784d04
Suryana <nanasuryana335@gmail.com>
2023-12-06 21:19:15 +0700

    feat(main.go): register new endpoint within it's handler

--- devflow/backend/main.go
+++ devflow/backend/main.go
@@ -30,7 +30,7 @@ func main() {
 	var (
 		userStore = db.NewMongoUserStore(client)
 		tagStore = db.NewMongoTagStore(client)
-		questionStore = db.NewMongoQuestionStore(client, tagStore)
+		questionStore = db.NewMongoQuestionStore(client, tagStore, userStore)
 
 		store = &db.Store{
 			Question: questionStore,
@@ -39,8 +39,8 @@ func main() {
 		}
 
 		questionHandler = api.NewQuestionHandler(store.Question, store.User, store.Tag)
-		userHandler = api.NewUserHandler(store.User)
-		tagHandler = api.NewTagHandler(store.Tag)
+		userHandler = api.NewUserHandler(store.User, store.Tag, store.Question)
+		tagHandler = api.NewTagHandler(store.Tag, store.User)
 		app = fiber.New(config)
 		auth = app.Group("/api")
 		apiv1 = app.Group("/api/v1")
@@ -49,20 +49,24 @@ func main() {
 	app.Use(cors.New())
 	// Question Handler
 	apiv1.Get("/question/:_id", questionHandler.HandleGetQuestionByID)
-	apiv1.Post("/ask-question", questionHandler.HandleAskQuestion)
 	apiv1.Get("/question", questionHandler.HandleGetQuestions)
+	apiv1.Get("/question/user/:_id", questionHandler.HandleGetQuestionsByUserID)
+	apiv1.Post("/ask-question", questionHandler.HandleAskQuestion)
+	apiv1.Delete("/question/:_id", questionHandler.HandleDeleteQuestionByID)
+	
 
 	// User Handler
 	// auth.Post("/sign-up", userHandler.HandleSignUp)
-	auth.Post("/sign-up", userHandler.HandleCreateUser)
 	apiv1.Get("/user/:clerkID", userHandler.HandleGetUserByID)
+	auth.Post("/sign-up", userHandler.HandleCreateUser)
 	apiv1.Put("/user/:clerkID", userHandler.HandleUpdateUser)
 	apiv1.Delete("/user/:clerkID", userHandler.HandleDeleteUser)
 
 	// Tag Handler
 	apiv1.Get("/tag/:_id", tagHandler.HandleGetTagByID)
-	apiv1.Post("/tag", tagHandler.HandleCreateTag)
 	apiv1.Get("/tag/:name", tagHandler.HandleGetTagByName)
+	apiv1.Get("/tag", tagHandler.HandleGetTags)
+	apiv1.Post("/tag", tagHandler.HandleCreateTag)
 	apiv1.Put("/tag/:_id", tagHandler.HandleUpdateTag)
 	
 
